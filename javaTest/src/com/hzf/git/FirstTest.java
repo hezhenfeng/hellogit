7cc92568f30a824bd7162eb11a8be3514ae03197
owen <1940086130@qq.com>
2021-06-20 16:17:41 +0800

    first

--- javaTest/src/com/hzf/git/FirstTest.java
+++ javaTest/src/com/hzf/git/FirstTest.java
@@ -4,6 +4,11 @@ public class FirstTest {
 
     public static void main(String[] args) {
         System.out.println("11");
+        check();
+    }
+
+    public static  void check(){
+        System.out.println("check");
     }
 
 }
